9afb86b05f19e994d2d7efc1100bf7173b4934bc
André Antunes <andre.r.antunes@ist.utl.pt>
2023-08-08 17:52:37 +0100

    Organize Level Scriptable Obj

--- Assets/Prefabs/Level.cs
+++ Assets/Prefabs/Level.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level", menuName = "ScriptableObjects/Level")]
 public class Level : ScriptableObject
 {
-    public bool rain;
-    public float rainInterval;
-    public GameObject[] obstacleList;
     public float levelLength;
-
-    [Header("Obstacles Probability")]
-    public float min;
-    public float max;
 
     [Header("Background")]
     public GameObject backgrounds;
 
     [Header("Music")]
     public AudioManager.MusicsRiver music;
+
+    [Header("Obstacles Variables")]
+    public GameObject[] obstacleList;
+    public float minProb;
+    public float maxProb;
+
+    [Header("Rain Variables")]
+    public bool rain;
+    public float rainInterval;
 }
