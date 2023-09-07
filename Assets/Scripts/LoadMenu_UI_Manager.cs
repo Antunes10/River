b38de8597806a0d9ed1acd6d4912f7062cfde02b
André Antunes <andre.r.antunes@ist.utl.pt>
2023-09-07 19:50:02 +0100

    Implement Load Menu

---
+++ Assets/Scripts/LoadMenu_UI_Manager.cs
@@ -0,0 +1,53 @@
+using Ink.Parsed;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static UnityEngine.JsonUtility;
+
+public class LoadMenu_UI_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] _loadButtons;
+
+    void Start()
+    {
+        int number = 0;
+        GameState _gs = null;
+        foreach (var button in _loadButtons) 
+        {
+            if(System.IO.File.Exists(Application.dataPath + "/Resources/RiverSave" + number + ".json"))
+            {
+                StreamReader sr = new StreamReader(Application.dataPath + "/Resources/RiverSave" + number + ".json");
+                string json = sr.ReadToEnd();
+                sr.Close();
+                _gs = FromJson<GameState>(json);
+
+                button.enabled = true;
+                button.GetComponentInChildren<TextMeshProUGUI>().text = _gs.date;
+
+                Initialization(button, number);
+            }
+            else
+            {
+                button.enabled = false;
+                button.GetComponentInChildren<TextMeshProUGUI>().text = null;
+            }
+
+            number++;
+        }
+    }
+
+    public void Initialization(Button button, int index)
+    {
+        button.onClick.AddListener(() => GameManager.Instance.LoadGame(index));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
